02378977d2432d088d55311ef1bfbf07d835b033
Reham236 <mslah3430@gmail.com>
2025-03-26 06:42:16 +0200

    Adding new Section

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function countdown(minutes) {
             let displaySeconds = seconds % 60;
 
             // تنسيق العرض ليكون بالشكل "دقائق:ثواني"
-            document.getElementById("countdown").innerHTML = `00:${displayMinutes}:${displaySeconds < 10 ? '0' : ''}${displaySeconds}`;
+            document.getElementById("timer").innerHTML = `00:${displayMinutes}:${displaySeconds < 10 ? '0' : ''}${displaySeconds}`;
             seconds--;
         } else {
             // إعادة العد التنازلي من 30 دقيقة
@@ -17,3 +17,29 @@ function countdown(minutes) {
 
 // الاستخدام:
 countdown(30); // يبدأ العد التنازلي من 30 دقيقة
+
+
+//Category Section
+document.addEventListener("DOMContentLoaded", function () {
+    const sliders = document.querySelectorAll(".categories-slider");
+    const controls = document.querySelectorAll(".controls");
+
+    controls.forEach((control, index) => {
+        const leftArrow = control.querySelector(".left-arrow");
+        const rightArrow = control.querySelector(".right-arrow");
+        const slider = sliders[index]; // يربط الأسهم بالقسم الصحيح
+
+        rightArrow.addEventListener("click", () => {
+            slider.scrollBy({ left: 500, behavior: "smooth" });
+        });
+
+        leftArrow.addEventListener("click", () => {
+            slider.scrollBy({ left: -500, behavior: "smooth" });
+        });
+    });
+});
+
+// redirect to checkout page
+document.getElementById("buyBtn").addEventListener("click", function() {
+    window.location.href = "checkout.html";
+});
